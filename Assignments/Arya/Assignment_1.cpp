46ce5ad9b1143c4e759462ff6ab2690678f2784e
Deepakarya76 <98823925+Deepakarya76@users.noreply.github.com>
2024-02-02 18:45:41 +0530

    Assignment_1.cpp

--- Assignments/Arya/Assignment_1.cpp
+++ Assignments/Arya/Assignment_1.cpp
@@ -14,32 +14,13 @@ int main(){
 
     for(int i = 1; i<=n;i++){
         for(int x=1;x<=n-i+1;x++){
-            cout<<x;
+            cout<<x<<" ";
         }
         for(int m=1;m<=2*(i-1);m++){
-            cout<<"*";
-        }
-        for(int z=n;z>=(i-1);z--){
-            cout<<z;
-
-        }
-        cout<<endl;
-    }
-}
-
-int main(){
-    int n;
-    cin>>n;
-
-    for(int i = 1; i<=n;i++){
-        for(int x=1;x<=n-i+1;x++){
-            cout<<x;
-        }
-        for(int m=1;m<=2*(i-1);m++){
-            cout<<"*";
+            cout<<"*"<<" ";
         }
         for(int z=n;z>=i;z--){
-            cout<<z-i+1;
+            cout<<z-i+1<<" ";
 
         }
         cout<<endl;
